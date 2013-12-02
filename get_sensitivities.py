deddbfacb0bdf7aa552d6f5d91f765ea596782c1
scott <scott@pomona>
2013-12-02 12:29:55 -0500

    actually functional sensitivity analysis

--- get_sensitivities.py
+++ get_sensitivities.py
@@ -6,47 +6,91 @@ the sensitivity of different positions for each rate curve to each
 of the modified parameters
 '''
 
+import csv
 import numpy as np
 import scipy.interpolate, scipy.stats
 
-vals = [0, 1, 2, 3, 4]
-rate_i = 0
+n_vals = 5
 
-positions = []
-for val in vals:
-    fn = 'data/rates_0_{0}.csv'.format(val)
-    dat = np.genfromtxt(fn, delimiter=',')
-    x = range(dat.shape[0])
-    y = dat[:, rate_i]
+def get_single_values(x, y):
+    '''give interpolated x positions of half max's, max'''
 
     f = scipy.interpolate.interp1d(x, y, kind='cubic')
 
     new_x = np.linspace(min(x), max(x), 1000)
     new_y = f(new_x)
-    print new_y
-    new_dat = np.vstack((new_x, new_y)).T
 
-    # find the value and position of the maximum
+    # find position of maximum
     max_pos = np.argmax(new_y)
     max_x = new_x[max_pos]
     max_y = np.max(new_y)
 
     # find the upper and lower half-max
-    above_hm = new_x[new_y > np.max(new_y) / 2]
-    uhm = np.max(above_hm)
-    lhm = np.min(above_hm)
+    if np.max(new_y) - np.min(new_y) == 0:
+        # escape hatch
+        return [0, 0, 0]
+    else:
+        above_hm = new_x[new_y > (np.max(new_y) - np.min(new_y)) / 2]
+        uhm = np.max(above_hm)
+        lhm = np.min(above_hm)
 
-    positions.append([val, lhm, max_x, uhm])
+        return [lhm, max_x, uhm]
 
-    np.savetxt('out_{0}'.format(val), new_dat)
+slope = lambda x, y: scipy.stats.linregress(x, y)[0]
+slopes = lambda x, ys: [slope(x, y) for y in ys]
 
-positions = np.array(positions)
+# read in the rate names
+with open('rate_names.txt', 'r') as f:
+    rate_names = [l.strip() for l in f.readlines()]
 
-# do the linear regressions for each column
-for i in [1, 2, 3]:
-    x = positions[:, 0]
-    y = positions[:, i]
-    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(x, y)
-    print i, slope
+# read in the parameters
+params = []
+with open('base_params.txt', 'r') as f:
+    r = csv.reader(f)
+    for i, param, base_val in r:
+        params.append(param)
 
-#np.savetxt('out', out)
+# loop over parameters
+out_rows = []
+for param_i, param_name in enumerate(params):
+    out_row = []
+
+    # get values of the parameter
+    fn = 'valmap_{0}.txt'.format(param_i)
+    with open(fn, 'r') as f:
+        r = csv.reader(f)
+        param_vals = np.array([row[1] for row in r], dtype=float)
+
+    # loop over values of the parameter
+    # initialize single values output
+    single_values = np.empty((len(rate_names), len(param_vals), 3))
+    for val_i, param_val in enumerate(param_vals):
+        # get the data
+        fn = 'data/rates_{0}_{1}.csv'.format(param_i, val_i)
+        dat = np.genfromtxt(fn, delimiter=',')
+        x = range(dat.shape[0])
+
+        # loop over each process
+        for rate_i, rate_name in enumerate(rate_names):
+            y = dat[:, rate_i]
+            single_values[rate_i, val_i, :] = np.array(get_single_values(x, y))
+
+    # do the linear regressions
+    for rate_i, rate_name in enumerate(rate_names):
+        for metric_i in [0, 1, 2]:
+            my_slope = slope(param_vals, single_values[rate_i, :, metric_i])
+            out_row.append(my_slope)
+            print 'p={} r={} m={} slope={}'.format(param_name, rate_name, metric_i, my_slope)
+
+    out_rows.append(out_row)
+
+# construct the output
+with open('out', 'w') as f:
+    w = csv.writer(f)
+
+    header = ['rate'] + ['{0}_{1}'.format(rate_name, pos_name) for rate_name in rate_names for pos_name in ['LHM', 'max', 'UHM']]
+    w.writerow(header)
+
+    for param_name, row in zip(params, out_rows):
+        new_row = [param_name] + row
+        w.writerow(new_row)
